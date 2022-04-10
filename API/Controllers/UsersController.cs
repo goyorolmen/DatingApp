7edcacd9dd5d7d821dc855151267e03ee1ceac32
goyorolmen <goyo_rolmen@yahoo.com>
2022-04-10 20:39:16 +0200

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    // este es el Controller que mostrará los datos de usuarios
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Se añaden endpoints para mostrar todos los usuarios de la DB
+        // y otro para añadir usuarios especificos.
+        
+        /**********
+            Se le llama mediante /api/users  en la direccion web
+            https://localhost:5001/api/users
+        ***********/
+        // Siempre que se usen Bases de Datos en paginas web las best practices
+        // son usar metodos asincronos
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUSers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        /**********
+            Obtener lista de un usuario buscado por Id
+            Se le llama mediante /api/users  en la direccion web
+            https://localhost:5001/api/users/3
+        ***********/
+        
+         [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUSer(int id)
+        {
+            return  await _context.Users.FindAsync(id);
+        }
+    }
+    
+}
