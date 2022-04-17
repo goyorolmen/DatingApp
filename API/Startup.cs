6c4a15a054c3d8518b3a272d14ee4fc078ca4b47
goyorolmen <goyo_rolmen@yahoo.com>
2022-04-17 11:30:30 +0200

    End Of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Infterfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +17,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+
 
 namespace API
 {
@@ -28,19 +35,23 @@ namespace API
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Se le llama Dependency Injection Containes y contiene los servicios que
+        // se quieren usar (Conexion a la Base de Datos/DbContex, seguridad, etc)
         public void ConfigureServices(IServiceCollection services)
         {
-            // Añadir la conexion a la Base de Datos
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));    
-            });
+            
+            // Servicios: Token y Base de Datos
+            services.AddApplicationServices(_config);
+
             services.AddControllers();
 
             // Este servicio de seguridad para el mecanismo de seguridad CORS
             // Impide/bloquea que se llamen servicios con origen distinto.
             // En este caso es localhost:4200 (Angular) llama a localhost:5001 (API)
             services.AddCors();
+
+            // Servicio Autenticacion 
+            services.AddIdentityServices(_config);
 
             services.AddSwaggerGen(c =>
             {
@@ -65,6 +76,9 @@ namespace API
             // El servicio CORS debe ir justo aqui, entre UseRouting y UseAuthorization
             // Permite cualquier metodo con origen en el request localhost/4200
             app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+            // Se añade la autenticacion justo antes de la autorizacion
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
